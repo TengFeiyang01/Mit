e727849b1c4fbdd7a311445cea7600f38a4c5641
TengFeiyang01 <2196442691@qq.com>
2023-12-09 15:50:52 +0800

    6.5840 lab1

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,15 +1,55 @@
 package mr
 
-import "log"
+import (
+	"log"
+	"sync"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
+type CoordinatorTaskStatus int
+type State int
+
+const (
+	Map = iota
+	Reduce
+	Wait
+	Exit
+)
+
+const (
+	Idle CoordinatorTaskStatus = iota // 未执行
+	InProgress
+	Completed
+)
+
+var mu sync.Mutex
+
+// Task 存储任务信息
+type Task struct {
+	Input         string // 该任务需要处理的文件 map 任务处理原数据 reduce 处理map产生的中间结果文件
+	TaskState     State  // 任务状态 Map | Reduce | Wait | Exit
+	NReducer      int
+	TaskNumber    int      // 任务编号 生成中间文件需要
+	Intermediates []string // 产生的中间文件的文件名列表
+}
+
+type CoordinatorTask struct {
+	TaskStatus    CoordinatorTaskStatus // CoordinatorTask的状态
+	TaskReference *Task                 // Task的指针
+	StartTime     time.Time             // Task的启动时间
+}
 
 type Coordinator struct {
-	// Your definitions here.
-
+	TaskQueue        chan *Task               // 等待执行的task
+	TaskMeta         map[int]*CoordinatorTask // 当前所有task的信息
+	CoordinatorPhase State                    // Coordinator 的阶段
+	NReduce          int                      // Reducer的个数
+	InputFiles       []string                 // 要分配給map处理的文件列表
+	Intermediates    [][]string               // Map任务产生的R个中间文件的信息
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -23,7 +63,6 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -46,25 +85,167 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
+	mu.Lock()
+	defer mu.Unlock()
+	ret := c.CoordinatorPhase == Exit
 	return ret
 }
 
-//
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// MakeCoordinator
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		TaskQueue:        make(chan *Task, max(len(files), nReduce)),
+		TaskMeta:         make(map[int]*CoordinatorTask),
+		CoordinatorPhase: Map,
+		NReduce:          nReduce,
+		InputFiles:       files,
+		Intermediates:    make([][]string, nReduce),
+	}
 
 	// Your code here.
+	// 创建 Map 任务
+	c.createMapTask()
 
-
+	// 一个程序启动coordinator 其他程序成为worker
+	// 这里是启动coordinate服务器
+	// 拥有coordinate代码的是coordinator 发送RPC过来的是worker
 	c.server()
+	// 启动一个goroutine检查Time Out 超市任务
+	go c.catchTimeOut()
 	return &c
+}
+
+// catchTimeOut 超时检测
+func (c *Coordinator) catchTimeOut() {
+	for {
+		// 每5秒检查一次
+		time.Sleep(5 * time.Second)
+		mu.Lock()
+		if c.CoordinatorPhase == Exit {
+			return
+		}
+		for _, coordinatorTask := range c.TaskMeta {
+			if coordinatorTask.TaskStatus == InProgress && time.Now().Sub(coordinatorTask.StartTime) > 10*time.Second {
+				c.TaskQueue <- coordinatorTask.TaskReference
+				coordinatorTask.TaskStatus = Idle
+			}
+		}
+		mu.Unlock()
+	}
+}
+
+// createMapTask 创建Map任务
+func (c *Coordinator) createMapTask() {
+	for idx, filename := range c.InputFiles {
+		taskMeta := Task{
+			Input:      filename,
+			TaskState:  Map,
+			NReducer:   c.NReduce,
+			TaskNumber: idx,
+		}
+		c.TaskQueue <- &taskMeta
+		// 数据表
+		c.TaskMeta[idx] = &CoordinatorTask{
+			TaskStatus:    Idle,
+			TaskReference: &taskMeta,
+		}
+	}
+}
+
+// createReduceTask 创建Reduce任务
+func (c *Coordinator) createReduceTask() {
+	c.TaskMeta = make(map[int]*CoordinatorTask)
+	// c.Intermediates 是一个二维列表 第一维是reduceTaskID 第二维是Map处理后得到的中间文件
+	// 遍历所有的中间结果 对每个中间结果去创建一个Task信息
+	for idx, files := range c.Intermediates {
+		taskMeta := Task{
+			TaskState:     Reduce,
+			NReducer:      c.NReduce,
+			TaskNumber:    idx,
+			Intermediates: files,
+		}
+		// 新的ReduceTask放入TaskQueue 等待分配
+		c.TaskQueue <- &taskMeta
+		c.TaskMeta[idx] = &CoordinatorTask{
+			TaskStatus:    Idle,
+			TaskReference: &taskMeta,
+		}
+	}
+}
+
+// AssignTask 等待worker调用 向 worker 发送任务
+func (c *Coordinator) AssignTask(args *ExampleArgs, reply *Task) error {
+	// assignTask就看看自己队列中是否还有task
+	mu.Lock()
+	defer mu.Unlock()
+	if len(c.TaskQueue) > 0 {
+		// 有任务 发出去
+		*reply = *<-c.TaskQueue
+		c.TaskMeta[reply.TaskNumber].TaskStatus = InProgress
+		// 记录启动时间 超时kill
+		c.TaskMeta[reply.TaskNumber].StartTime = time.Now()
+	} else if c.CoordinatorPhase == Exit {
+		*reply = Task{TaskState: Exit}
+	} else {
+		// 没有task可分配 worker等待
+		// eg: 两个task 三个worker 第三个worker没有任务 但是此时并没有结束 他(worker)不能退出 应该让其等待
+		*reply = Task{TaskState: Wait}
+	}
+	return nil
+}
+
+func (c *Coordinator) TaskCompleted(task *Task, reply *ExampleReply) error {
+	// 更新task状态
+	mu.Lock()
+	defer mu.Unlock()
+	//
+	if task.TaskState != c.CoordinatorPhase || c.TaskMeta[task.TaskNumber].TaskStatus == Completed {
+		// 因为worker卸载同一个磁盘上 对应同一个结果要丢弃
+		return nil
+	}
+	c.TaskMeta[task.TaskNumber].TaskStatus = Completed
+	go c.processTaskResult(task)
+	return nil
+}
+
+func (c *Coordinator) processTaskResult(task *Task) {
+	mu.Lock()
+	defer mu.Unlock()
+	switch task.TaskState {
+	case Map:
+		// 收集 Map 任务的 intermediate信息
+		for reduceTaskID, filePath := range task.Intermediates {
+			c.Intermediates[reduceTaskID] = append(c.Intermediates[reduceTaskID], filePath)
+		}
+		// 所有的Map任务处理结束后 进入Reduce阶段
+		if c.allTaskDone() {
+			c.createReduceTask()
+			c.CoordinatorPhase = Reduce
+		}
+	case Reduce:
+		if c.allTaskDone() {
+			// 获得所有reduce task之后 退出
+			c.CoordinatorPhase = Exit
+		}
+	}
+}
+
+func (c *Coordinator) allTaskDone() bool {
+	for _, task := range c.TaskMeta {
+		if task.TaskStatus != Completed {
+			return false
+		}
+	}
+	return true
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,17 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
-
 
 //
 // Map functions return a slice of KeyValue.
@@ -13,9 +20,15 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
-// use ihash(key) % NReduce to choose the reduce
+// use ihash(key) % NReducer to choose the reduce
 // task number for each KeyValue emitted by Map.
 //
 func ihash(key string) int {
@@ -24,21 +37,28 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
+	// 启动worker
+	for {
+		// worker从master获取任务
+		task := getTask()
 
-//
-// main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
-
-	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
-
+		// 拿到task后，根据task的State, Map | Reduce Map交给mapper Reduce交给reducer
+		// 额外加两个状态 等待/退出
+		switch task.TaskState {
+		case Map:
+			mapper(&task, mapf)
+		case Reduce:
+			reducer(&task, reducef)
+		case Wait:
+			time.Sleep(5 * time.Second)
+		case Exit:
+			return
+		}
+	}
 }
 
-//
+// CallExample
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
@@ -65,6 +85,121 @@ func CallExample() {
 	} else {
 		fmt.Printf("call failed!\n")
 	}
+}
+
+func getTask() Task {
+	args := ExampleArgs{}
+	reply := Task{}
+	// call 函数使用反射来调用名为 "Coordinator.AssignTask" 的方法，其中 args 是传递给方法的参数，reply 是方法执行后的结果。
+	call("Coordinator.AssignTask", &args, &reply)
+	return reply
+}
+
+func TaskCompleted(task *Task) {
+	// 通过 RPC， 把task信息发送给coordinator
+	reply := ExampleReply{}
+	call("Coordinator.TaskCompleted", task, &reply)
+}
+
+func mapper(task *Task, mapf func(string, string) []KeyValue) {
+	//从文件名读取content
+	content, err := ioutil.ReadFile(task.Input)
+	if err != nil {
+		log.Fatal("Failed to read file: "+task.Input, err)
+	}
+	// 将content交给mapf，缓存结果
+	intermediates := mapf(task.Input, string(content))
+
+	// 缓存后的结果会写到本地磁盘，并切成R份
+	// 切分方式是根据key做hash
+	bucket := make([][]KeyValue, task.NReducer)
+	for _, intermediate := range intermediates {
+		slot := ihash(intermediate.Key) % task.NReducer
+		bucket[slot] = append(bucket[slot], intermediate)
+	}
+
+	mapOutput := make([]string, 0)
+	for i := 0; i < task.NReducer; i++ {
+		mapOutput = append(mapOutput, writeToLocalFile(task.TaskNumber, i, &bucket[i]))
+	}
+
+	// R个文件的位置发送给coordinator
+	task.Intermediates = mapOutput
+	TaskCompleted(task)
+}
+
+func reducer(task *Task, reducef func(string, []string) string) {
+	// 先从filepath读取intermediate的KeyValue
+	intermediate := *readFromLocalFile(task.Intermediates)
+	// 根据kv排序
+	sort.Sort(ByKey(intermediate))
+
+	dir, _ := os.Getwd()
+	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("Failed to create temp file", err)
+	}
+	// 这部分代码修改自mrsequential.go
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		// 交给reducef，拿到结果
+		output := reducef(intermediate[i].Key, values)
+		// 写入到对应的output文件
+		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	tempFile.Close()
+	oname := fmt.Sprintf("mr-out-%v", task.TaskNumber)
+	os.Rename(tempFile.Name(), oname)
+	TaskCompleted(task)
+}
+
+// writeToLocalFile 写入临时文件 并返回临时文件的目录列表
+func writeToLocalFile(x int, y int, kvs *[]KeyValue) string {
+	dir, _ := os.Getwd()
+	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("Failed to create temp file", err)
+	}
+	enc := json.NewEncoder(tempFile)
+	for _, kv := range *kvs {
+		if err := enc.Encode(&kv); err != nil {
+			log.Fatal("Failed to write kv pair", err)
+		}
+	}
+	tempFile.Close()
+	outputName := fmt.Sprintf("mr-%d-%d", x, y)
+	os.Rename(tempFile.Name(), outputName)
+	return filepath.Join(dir, outputName)
+}
+
+// readFromLocalFile 从临时文件中读取所有的KeyValue pair 返回给reducer处理
+func readFromLocalFile(files []string) *[]KeyValue {
+	kva := []KeyValue{}
+	for _, filename := range files {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatal("Failed to open file"+filename, err)
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+		file.Close()
+	}
+	return &kva
 }
 
 //
